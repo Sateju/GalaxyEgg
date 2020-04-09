3a4ec908bb75481657b8b47cd3cbcf034363e531
Sateju <juan40ts@gmail.com>
2020-04-09 10:50:22 +0200

    remove unnecessary layout of main activity

--- app/src/main/java/lincete/galaxyegg/ui/MainActivity.kt
+++ app/src/main/java/lincete/galaxyegg/ui/MainActivity.kt
@@ -2,19 +2,15 @@ package lincete.galaxyegg.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import lincete.galaxyegg.R
-import lincete.galaxyegg.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
         val navController = findNavController(R.id.navHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
