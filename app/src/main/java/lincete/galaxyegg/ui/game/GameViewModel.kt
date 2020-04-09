0f3018e6d4ac466a31086fc011e35aa692f4dcb5
Sateju <juan40ts@gmail.com>
2020-04-09 12:49:21 +0200

    start with coroutines

--- app/src/main/java/lincete/galaxyegg/ui/game/GameViewModel.kt
+++ app/src/main/java/lincete/galaxyegg/ui/game/GameViewModel.kt
@@ -2,12 +2,52 @@ package lincete.galaxyegg.ui.game
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.*
 import lincete.galaxyegg.data.database.EggDatabaseDao
+import lincete.galaxyegg.data.database.EggEntity
 
 class GameViewModel(
-        val eggDao: EggDatabaseDao,
+        val database: EggDatabaseDao,
         application: Application) : AndroidViewModel(application) {
 
+    /** Coroutine variables */
+
+    /**
+     * viewModelJob allows us to cancel all coroutines started by this ViewModel.
+     */
+    private var viewModelJob = Job()
+
+    /**
+     * A [CoroutineScope] keeps track of all coroutines started by this ViewModel.
+     *
+     * Because we pass it [viewModelJob], any coroutine started in this uiScope can be cancelled
+     * by calling `viewModelJob.cancel()`
+     *
+     * By default, all coroutines started in uiScope will launch in [Dispatchers.Main] which is
+     * the main thread on Android. This is a sensible default because most coroutines started by
+     * a [ViewModel] update the UI after performing some processing.
+     */
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private var egg = MutableLiveData<EggEntity?>()
+
+    init {
+        initializeEgg()
+    }
+
+    private fun initializeEgg() {
+        uiScope.launch {
+            egg.value = getEggFromDatabase()
+        }
+    }
+
+    private suspend fun getEggFromDatabase(): EggEntity? {
+        return withContext(Dispatchers.IO) {
+            database.getEggCount()
+        }
+    }
 
     fun onVolumeChanged() {
 
@@ -15,5 +55,16 @@ class GameViewModel(
 
     fun onEggClicked() {
 
+    }
+
+    /**
+     * Called when the ViewModel is dismantled.
+     * At this point, we want to cancel all coroutines;
+     * otherwise we end up with processes that have nowhere to return to
+     * using memory and resources.
+     */
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
     }
 }
